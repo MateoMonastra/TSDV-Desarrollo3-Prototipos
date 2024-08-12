880961f2af24af3f4d52ceeb3999445fdee5820a
MateoMonastra <mateomonastravm@gmail.com>
2024-08-12 16:45:46 -0300

    Add vacuumcleaner script

---
+++ TSDV-Desarrollo3-Prototipos/Assets/Scripts/Player/VacuumCleaner.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Player
+{
+    public class VacuumCleaner : MonoBehaviour
+    {
+    
+    
+    
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
