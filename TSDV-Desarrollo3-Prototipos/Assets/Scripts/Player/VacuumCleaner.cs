ca7518a717354af9876536267c9fd5847c43f587
MateoMonastra <mateomonastravm@gmail.com>
2024-08-15 12:53:27 -0300

    Add wallcheck for vacuumcleaner

--- TSDV-Desarrollo3-Prototipos/Assets/Scripts/Player/VacuumCleaner.cs
+++ TSDV-Desarrollo3-Prototipos/Assets/Scripts/Player/VacuumCleaner.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Player
@@ -7,12 +8,26 @@ namespace Player
         [SerializeField] private Transform target;
         [SerializeField] private float speed;
         [SerializeField] private float maxAngle = 45.0f;
+        [SerializeField] private float renderDistance = 5.0f;
+        [SerializeField] private LayerMask wallLayer;
 
         private Collider _collider;
+        private Vector3? _collision;
+        private Ray _ray;
+        Quaternion left ;
+        Quaternion right ;
 
+        Vector3     leftBoundary;
+        Vector3     rightBoundary;
         private void Start()
         {
             _collider = GetComponent<Collider>();
+            
+            left = Quaternion.AngleAxis(-maxAngle, Vector3.up);
+            right = Quaternion.AngleAxis(maxAngle, Vector3.up);
+            leftBoundary = left * target.forward;
+            rightBoundary = right * target.forward;
+            transform.localScale = new Vector3(Mathf.Abs(((leftBoundary.x*2) * renderDistance) ), transform.localScale.y, transform.localScale.z);
         }
 
         public void TurnOn()
@@ -27,29 +42,43 @@ namespace Player
 
         private void OnTriggerStay(Collider other)
         {
-                var angleToObject = Vector3.Angle(target.forward, other.transform.position - target.position);
-                if (angleToObject <= maxAngle)
+            var angleToObject = Vector3.Angle(target.forward, other.transform.position - target.position);
+            if (angleToObject <= maxAngle)
+            {
+                _ray = new Ray(target.position,  other.transform.position - target.position);
+
+                if (Physics.Raycast(_ray, out RaycastHit hit, renderDistance, wallLayer))
                 {
-                    other.transform.position = Vector3.MoveTowards(other.transform.position, target.position,
-                        speed * Time.deltaTime);
+                    _collision = hit.point;
+                    return;
                 }
+
+                other.transform.position = Vector3.MoveTowards(other.transform.position, target.position,
+                    speed * Time.deltaTime);
+                _collision = null;
+            }
         }
 #if UNITY_EDITOR
 
         private void OnDrawGizmos()
         {
+            left = Quaternion.AngleAxis(-maxAngle, Vector3.up);
+            right = Quaternion.AngleAxis(maxAngle, Vector3.up);
+            leftBoundary = left * target.forward;
+            rightBoundary = right * target.forward;
+            
+            
             Gizmos.color = Color.green;
-            Gizmos.DrawLine(target.position, target.position + target.forward * 5);
-        
-            Quaternion Left = Quaternion.AngleAxis(-maxAngle, Vector3.up);
-            Quaternion Right = Quaternion.AngleAxis(maxAngle, Vector3.up);
-        
-            Vector3 leftBoundary = Left * target.forward;
-            Vector3 rightBoundary = Right * target.forward;
-        
-            Gizmos.DrawLine(target.position, target.position + leftBoundary * 5);
-        
-            Gizmos.DrawLine(target.position, target.position + rightBoundary * 5);
+            Gizmos.DrawLine(target.position, target.position + target.forward * renderDistance);
+
+            Gizmos.DrawLine(target.position, target.position + leftBoundary * renderDistance);
+
+            Gizmos.DrawLine(target.position, target.position + rightBoundary * renderDistance);
+            Gizmos.DrawRay(_ray);
+            if (_collision != null)
+            {
+                Gizmos.DrawSphere(_collision.Value, 0.5f);
+            }
         }
 #endif
     }
